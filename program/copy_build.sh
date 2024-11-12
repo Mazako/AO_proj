462023191e441f89aac066d9cbe04793ccbba7ab
apox122 <apox122@gmail.com>
2024-11-12 13:29:06 +0100

    delete .exe

--- program/copy_build.sh
+++ program/copy_build.sh
@@ -5,7 +5,7 @@ DESTINATION="MillerRabin"
 
 if [ -f "$SOURCE" ]; then
     cp -f "$SOURCE" "$DESTINATION"
-    echo "Plik został skopiowany do katalogu 'build'."
+    echo "Plik został skopiowany do katalogu 'program'."
 else
     echo "Plik $SOURCE nie istnieje."
 fi
