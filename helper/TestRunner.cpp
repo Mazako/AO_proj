61fa0848739429314b2f5ddfaea61c9fd00f1dd0
michal.maziarz <michal.maziarz12@gmail.com>
2024-11-17 16:24:55 +0100

    better readfile and unified measure counting

--- helper/TestRunner.cpp
+++ helper/TestRunner.cpp
@@ -11,47 +11,41 @@
 
 void run_m_cpu_tests(uint64_t* numbers, int count, int iterations) {
     std::cout << "----------M_CPU TESTS----------\n";
-    for (int i = 0; i < count; ++i) {
-        MultiThreadedMillerRabinTest test(numbers[i], iterations);
-        const auto [time, is_prime] = Utils::measure_time<bool>([&test]() { return test.is_prime(); });
-        std::cout << "Number " << numbers[i] << (is_prime ? " PRIME" : " COMPOSITE") << "\n";
-        std::cout << "Time: " << time << " ms\n";
-    }
+
+    auto [time, _] = Utils::measure_time<void*>([numbers, count, iterations] () -> void* {
+        for (int i = 0; i < count; ++i) {
+            MultiThreadedMillerRabinTest test(numbers[i], iterations);
+            bool is_prime = test.is_prime();
+            std::cout << "Number " << numbers[i] << (is_prime ? " PRIME" : " COMPOSITE") << "\n";
+        }
+        return nullptr;
+    });
+    std::cout << "Time: " << time << " ms\n";
 }
 
 void run_t_cpu_tests(uint64_t* numbers, int count, int iterations) {
     std::cout << "----------T_CPU TESTS----------\n";
-    for (int i = 0; i < count; ++i) {
-        SingleThreadedMillerRabinTest test(numbers[i], iterations);
-        const auto [time, is_prime] = Utils::measure_time<bool>([&test]() { return test.is_prime(); });
-        std::cout << "Number " << numbers[i] << (is_prime ? " PRIME" : " COMPOSITE") << "\n";
-        std::cout << "Time: " << time << " ms\n";
-    }
+    auto [time, _] = Utils::measure_time<void*>([numbers, count, iterations] () -> void* {
+        for (int i = 0; i < count; ++i) {
+            SingleThreadedMillerRabinTest test(numbers[i], iterations);
+            bool is_prime = test.is_prime();
+            std::cout << "Number " << numbers[i] << (is_prime ? " PRIME" : " COMPOSITE") << "\n";
+        }
+        return nullptr;
+    });
+    std::cout << "Time: " << time << " ms\n";
 }
 
 void run_gpu_tests(uint64_t* numbers, int count, int iterations) {
     std::cout << "----------GPU TESTS----------\n";
-    for (int i = 0; i < count; ++i) {
-        cudaEvent_t start, stop;
-        cudaEventCreate(&start);
-        cudaEventCreate(&stop);
-
-        cudaEventRecord(start);
-        bool is_prime = miller_rabin_test_gpu(numbers[i], iterations);
-
-        cudaEventRecord(stop);
-        cudaEventSynchronize(stop);
-
-        float elapsedTime;
-        cudaEventElapsedTime(&elapsedTime, start, stop);
-        int roundedTime = static_cast<int>(std::round(elapsedTime));
-
-        std::cout << "GPU: Number " << numbers[i] << (is_prime ? " PRIME" : " COMPOSITE") << "\n";
-        std::cout << "Time: " << roundedTime << " ms\n";
-
-        cudaEventDestroy(start);
-        cudaEventDestroy(stop);
-    }
+    auto [time, _] = Utils::measure_time<void*>([numbers, count, iterations] () -> void* {
+        for (int i = 0; i < count; ++i) {
+            bool is_prime = miller_rabin_test_gpu(numbers[i], iterations);
+            std::cout << "GPU: Number " << numbers[i] << (is_prime ? " PRIME" : " COMPOSITE") << "\n";
+        }
+        return nullptr;
+    });
+    std::cout << "Time: " << time << " ms\n";
 }
 
 void run_batch_gpu_test(uint64_t* numbers, int count, int iterations) {
