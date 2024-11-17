b6851f06d53a326a0972106198d6d6a23d4e22c4
Mazako <99810330+Mazako@users.noreply.github.com>
2024-11-17 15:50:56 +0100

    Stream test (#8)
    * separated number decomposition
    
    * Steamed data flow
    
    * Streamed data flow
    
    * cleanup

--- miller-cuda/MillerRabinMultipleNumberExecutor.cu
+++ miller-cuda/MillerRabinMultipleNumberExecutor.cu
@@ -1,10 +1,16 @@
 #include "MillerRabinMultipleNumberExecutor.cuh"
 
 #include <atomic>
-#include <cstdio>
 #include <curand_kernel.h>
-#include <sys/stat.h>
 #include "Utils.h"
+
+__global__ void decompose_number_kernel(uint64_t* numbers, uint64_t* exponent_of_twos, uint64_t* odd_parts, int n) {
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if (idx < n) {
+        auto number = numbers[idx];
+        Utils::decompose_number(number - 1, exponent_of_twos[idx], odd_parts[idx]);
+    }
+}
 
 __global__ void init_curand_state_kernel(curandState* states, int* results, int n, int iterations) {
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
@@ -17,7 +23,7 @@ __global__ void init_curand_state_kernel(curandState* states, int* results, int 
     }
 }
 
-__global__ void test_kernel(uint64_t* numbers, int* results, curandState* curand_states, int n, int iterations) {
+__global__ void test_kernel(uint64_t* numbers, uint64_t* exponent_of_twos, uint64_t* odd_parts, int* results, curandState* curand_states, int n, int iterations) {
     int number_idx = blockIdx.y;
     int iteration_idx = threadIdx.x + blockIdx.x * blockDim.x;
 
@@ -27,8 +33,8 @@ __global__ void test_kernel(uint64_t* numbers, int* results, curandState* curand
     }
 
     auto const base = cuda_128_specific_random(&curand_states[number_idx * iteration_idx], 2, numbers[number_idx] - 1);
-    uint64_t exponent_of_two, odd_part;
-    Utils::decompose_number(numbers[number_idx] - 1, exponent_of_two, odd_part);
+    auto const odd_part = odd_parts[number_idx];
+    auto const exponent_of_two = exponent_of_twos[number_idx];
 
     uint64_t x = Utils::modular_pow(base, odd_part, numbers[number_idx]);
 
@@ -55,26 +61,37 @@ int* miller_rabin_test_gpu_multiple(uint64_t* numbers, int n, int iterations, in
     int* h_results = (int*)malloc(results_size);
 
     int* d_results;
+    uint64_t* d_odd_parts;
+    uint64_t* d_exponent_of_twos;
     uint64_t* d_numbers;
     curandState* d_curand_states;
+    cudaStream_t stream;
 
     cudaMalloc(&d_numbers, arr_size);
     cudaMalloc(&d_results, results_size);
     cudaMalloc(&d_curand_states, n * iterations * sizeof(curandState));
+    cudaMalloc(&d_odd_parts, arr_size);
+    cudaMalloc(&d_exponent_of_twos, arr_size);
 
-    cudaMemcpy(d_numbers, numbers, results_size, cudaMemcpyHostToDevice);
+    cudaStreamCreate(&stream);
 
-    init_curand_state_kernel<<< ((n * iterations) + threads_per_block - 1) / threads_per_block, threads_per_block>>>(d_curand_states, d_results, n, iterations);
-    cudaDeviceSynchronize();
+    cudaMemcpyAsync(d_numbers, numbers, results_size, cudaMemcpyHostToDevice, stream);
+
+    init_curand_state_kernel<<< ((n * iterations) + threads_per_block - 1) / threads_per_block, threads_per_block, 0, stream>>>(d_curand_states, d_results, n, iterations);
+
+    decompose_number_kernel<<<(n + threads_per_block - 1) / threads_per_block, threads_per_block, 0, stream>>>(d_numbers, d_exponent_of_twos, d_odd_parts, n);
 
     auto blockDim = dim3((iterations + threads_per_block - 1) / threads_per_block, n);
-    test_kernel <<<blockDim, threads_per_block>>>(d_numbers, d_results, d_curand_states, n, iterations);
-    cudaDeviceSynchronize();
+    test_kernel<<<blockDim, threads_per_block, 0, stream>>>(d_numbers, d_exponent_of_twos, d_odd_parts, d_results, d_curand_states, n, iterations);
+    cudaMemcpyAsync(h_results, d_results, results_size, cudaMemcpyDeviceToHost, stream);
 
-    cudaMemcpy(h_results, d_results, results_size, cudaMemcpyDeviceToHost);
+    cudaStreamSynchronize(stream);
 
+    cudaStreamDestroy(stream);
     cudaFree(d_numbers);
     cudaFree(d_results);
     cudaFree(d_curand_states);
+    cudaFree(d_numbers);
+    cudaFree(d_exponent_of_twos);
     return h_results;
 }
--- miller-cuda/MillerRabinMultipleNumberExecutor.cuh
+++ miller-cuda/MillerRabinMultipleNumberExecutor.cuh
@@ -9,9 +9,11 @@ __device__ inline uint64_t cuda_128_specific_random(curandState* state, uint64_t
     return curand(state) % (max - min + 1) + min;
 }
 
+__global__ void decompose_number_kernel(uint64_t* numbers, uint64_t* exponents_of_twos, uint64_t* odd_parts, int n);
+
 __global__ void init_curand_state_kernel(curandState* states, int* results, int n, int iterations);
 
-__global__ void test_kernel(uint64_t* numbers, bool* results, curandState* curand_states, int n, int iterations);
+__global__ void test_kernel(uint64_t* numbers, uint64_t* exponent_of_twos, uint64_t* odd_parts, int* results, curandState* curand_states, int n, int iterations);
 
 int* miller_rabin_test_gpu_multiple(uint64_t* numbers, int n, int iterations, int threads_per_block = 128);
 
